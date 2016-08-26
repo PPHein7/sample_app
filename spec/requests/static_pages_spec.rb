2e9c097f64142f4bbc186cbe43114e329a86a4c0
Pyae Phyo Hein <pyae.phyo.hein@jp.fujitsu.com>
2016-08-26 14:57:58 +0900

    Add user following

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,35 +1,32 @@
-require 'rails_helper'
+require 'spec_helper'
 
-RSpec.describe "StaticPages", type: :request do
+describe "Static pages" do
  
   subject { page }
  
   shared_examples_for "all static pages" do
-    it { is_expected.to have_content(heading) }
-    it { is_expected.to have_title(full_title(page_title)) }
+    it { should have_content(heading) }
+    it { should have_title(full_title(page_title)) }
   end
-
-  
-
-  describe "Home page" do 
+  describe "Home page" do
     before { visit root_path }
+    let(:heading)    { 'Sample App' }
+    let(:page_title) { '' }
 
-    let(:heading) {'Sample App'}
-    let(:page_title) {''}
+    it_should_behave_like "all static pages"
+    it { should_not have_title('| Home') }
+    end
 
-    it_behaves_like "all static pages"
-    it { is_expected.not_to have_title('| Home') }
-
-    describe "for signed-in users" do
-      let(:user){ FactoryGirl.create(:user) }
+       describe "for signed-in users" do
+      let(:user) { FactoryGirl.create(:user) }
       before do
         FactoryGirl.create(:micropost, user: user, content: "Lorem")
         FactoryGirl.create(:micropost, user: user, content: "Ipsum")
         sign_in user
         visit root_path
       end
-
-      it "should render the user's feed" do 
+      
+      it "should render the user's feed" do
         user.feed.each do |item|
           expect(page).to have_selector("li##{item.id}", text: item.content)
         end
@@ -42,49 +39,45 @@ RSpec.describe "StaticPages", type: :request do
           visit root_path
         end
 
-        it { is_expected.to have_link("0 following", href: following_user_path(user)) }
-        it { is_expected.to have_link("1 followers", href: followers_user_path(user)) }
+        it { should have_link("0 following", href: following_user_path(user)) }
+        it { should have_link("1 followers", href: followers_user_path(user)) }
       end
-    end
   end
 
-  describe "Help page" do 
+  describe "Help page" do
     before { visit help_path }
 
-    let(:heading) {'Help'}
-    let(:page_title) {'Help'}
-    it_behaves_like "all static pages"
+    it { should have_content('Help') }
+    it { should have_title(full_title('Help')) }
   end
 
   describe "About page" do
     before { visit about_path }
 
-    let(:heading) {'About Us'}
-    let(:page_title) {'About Us'}
-    it_behaves_like "all static pages"
+    it { should have_content('About') }
+    it { should have_title(full_title('About Us')) }
   end
 
-  describe "Contact" do
+  describe "Contact page" do
     before { visit contact_path }
 
-    let(:heading) {'Contact'}
-    let(:page_title) {'Contact'}
-    it_behaves_like "all static pages"
+    it { should have_content('Contact') }
+    it { should have_title(full_title('Contact')) }
   end
 
-  it "should have the right links on the layout" do
+   it "should have the right links on the layout" do
     visit root_path
     click_link "About"
     expect(page).to have_title(full_title('About Us'))
     click_link "Help"
-    expect(page).to have_title(full_title('Help'))
+    expect(page).to # ここにコードを記入
     click_link "Contact"
-    expect(page).to have_title(full_title('Contact'))
+    expect(page).to # ここにコードを記入
     click_link "Home"
     click_link "Sign up now!"
-    expect(page).to have_title(full_title('Sign up'))
+    expect(page).to # ここにコードを記入
     click_link "sample app"
-    expect(page).to have_title(full_title(''))
+    expect(page).to # ここにコードを記入
   end 
 end
 
